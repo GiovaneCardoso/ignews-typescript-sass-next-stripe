2f605f206f28f1793754e66847304ce63825565f
GiovaneCardoso <giovane.cardoso@corebiz.ag>
2022-03-08 21:17:30 -0300

    integrate login to github and stripe - ssr and ssg

--- src/components/Header/SignInButton/index.tsx
+++ src/components/Header/SignInButton/index.tsx
@@ -1,23 +1,28 @@
 import React from 'react'
 import { FaGithub } from 'react-icons/fa'
+import { signIn, useSession, signOut } from 'next-auth/react'
 import { FiX } from 'react-icons/fi'
 import styles from './styles.module.scss'
 
 export const SignInButton = () => {
-  const isUserLoggedIn = true;
+  // const isUserLoggedIn = false;
+  const { data: session } = useSession()
+  // console.log("session", loggedIn)
+  // const session = useSession()
+  console.log("session", session)
 
-   return isUserLoggedIn ? (
+   return session?.user ? (
     (
-      <button className={styles.signInButton}>
-        <FaGithub color='#eba417'/>
-        Olá Giovane
+      <button className={styles.signInButton} onClick={()=> signOut()}>
+        <FaGithub color='#04d361'/>
+        Olá {session?.user?.name}
         <FiX color='#737380' className={styles.closeIcon} />
       </button>
     )
    ) : (
     (
-      <button className={styles.signInButton}>
-        <FaGithub color='#eba417'/>
+      <button className={styles.signInButton} onClick={() => signIn('github')}>
+        <FaGithub color='#eba417' />
         Entrar com Github
       </button>
     )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,17 @@
 import Head from 'next/head'
+import { GetStaticProps } from 'next'
 import SubscribeButton from '../components/SubscribeButton'
 import styles from './home.module.scss'
-export default function Home() {
+import { stripe } from '../services/stripe'
+
+interface HomeProps {
+  product: {
+    priceId: string,
+    amount: number,
+  }
+}
+
+export default function Home({ product: { priceId, amount } }: HomeProps ) {
 return (
     <>
       <Head>
@@ -10,15 +20,56 @@ return (
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
           <span>👏 Hey, Bem-vindo</span>
-          <h1>Novidades sobre o mundo do <span>React</span></h1>
+          <h1>Novidades sobre o mundo do <span>NextJS</span></h1>
           <p>
             Garanta seu acesso a todos os conteúdos
-            <span>por apenas R$9,90/mês</span>
+            <span>por apenas {amount}/mês</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={priceId}/>
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   )
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+
+  const price = await stripe.prices.retrieve('price_1Kb6onG2pINkmTxlnoV1Dc3G')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('pt-br', {
+      style: 'currency',
+      currency: 'BRL'
+      
+    }).format(price.unit_amount! / 100)
+  }
+
+  return {
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24 // 1 dia
+  } 
+}
+// export const getServerSideProps: GetServerSideProps = async () => {
+
+//   const price = await stripe.prices.retrieve('price_1Kb6onG2pINkmTxlnoV1Dc3G')
+
+//   const product = {
+//     priceId: price.id,
+//     amount: new Intl.NumberFormat('pt-br', {
+//       style: 'currency',
+//       currency: 'BRL'
+      
+//     }).format(price.unit_amount! / 100)
+//   }
+
+//   return {
+//     props: {
+//       product
+//     }
+//   }
+// }
+
