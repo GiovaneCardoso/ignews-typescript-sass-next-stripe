c5e0a6e061dc4b60ccbdc214f38fdd34e3711c30
GiovaneCardoso <giovane.cardoso@corebiz.ag>
2022-04-10 22:53:26 -0300

    integrate faunadb with stripe and setting up payment methods

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,13 +1,43 @@
+import { signIn, useSession } from 'next-auth/react'
 import React from 'react'
+import { api } from '../../services/api'
+import { getStripeJs } from '../../services/stripe-js'
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
   priceId: string
 }
 
-const SubscribeButton = ({priceId}: SubscribeButtonProps) => {
+const SubscribeButton = ({ priceId }: SubscribeButtonProps) => {
+  
+  const { data: session } = useSession()
+
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn('github')
+      return
+    }
+
+    try {
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data
+
+      const stripe = await getStripeJs()
+      await stripe?.redirectToCheckout({ sessionId })
+
+    } catch {
+
+    }
+
+  }
+
   return (
-    <button type='button' className={styles.subscribeButton}>
+    <button type='button'
+      onClick={handleSubscribe}
+      className={styles.subscribeButton}
+    >
         Insrever-se
     </button>
   )
